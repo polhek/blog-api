126478af4993a4282420857b81e01d3d22a7726e
polhek <ziga.groselj@gmail.com>
2021-04-14 19:00:17 +0200

    fix

--- passport/passport.js
+++ passport/passport.js
@@ -1,14 +1,13 @@
 const passport = require('passport');
 const { ExtractJwt } = require('passport-jwt');
 const JwtStrategy = require('passport-jwt').Strategy;
-//const JwtStrategy = require('passport-jwt').Strategy;
-const User = require('../models/user');
 require('dotenv').config();
+const User = require('../models/user');
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.secretOrKey;
-
+console.log(opts.secretOrKey);
 const strategy = new JwtStrategy(opts, (payload, done) => {
   User.findOne({ _id: payload.sub })
     .then((user) => {
