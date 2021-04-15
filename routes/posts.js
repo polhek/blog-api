edc280f5f72705a324ea094ae1e11d0fa547b8ab
polhek <ziga.groselj@gmail.com>
2021-04-15 15:50:42 +0200

    Protected some routes to use them with jwt

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postsController');
-
+const passport = require('passport');
 /* GET home page. */
 router.get('/', postController.allPosts_GET);
 
-router.post('/create', postController.newPost_POST);
+router.post(
+  '/create',
+  passport.authenticate('jwt', { session: false }),
+  postController.newPost_POST
+);
 
 router.get('/:id', postController.getSpecifiedPost_GET);
 
-router.put('/:id/update', postController.editSpecifiedPost_PUT);
+router.put(
+  '/:id/update',
+  passport.authenticate('jwt', { session: false }),
+  postController.editSpecifiedPost_PUT
+);
 
-router.delete('/:id/delete', postController.deleteSpecifiedPost_DELETE);
+router.delete(
+  '/:id/delete',
+  passport.authenticate('jwt', { session: false }),
+  postController.deleteSpecifiedPost_DELETE
+);
 
 module.exports = router;
